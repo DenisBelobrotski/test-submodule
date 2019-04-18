7bc271b75eac937c959b485b950c867530c3440f
Denis Belobrotski <denis.b@playgendary.com>
2019-04-18 12:09:26 +0300

    initial commit

---
+++ include/libtestparser/libtestparser.h
@@ -0,0 +1,10 @@
+//
+// Created by user on 2019-04-17.
+//
+
+#ifndef APP_PROJECT_TEST_H
+#define APP_PROJECT_TEST_H
+
+void sayHello();
+
+#endif //APP_PROJECT_TEST_H
---
+++ src/libtestparser.cpp
@@ -0,0 +1,11 @@
+//
+// Created by user on 2019-04-17.
+//
+
+#include <iostream>
+#include <libtestparser/libtestparser.h>
+
+void sayHello()
+{
+    std::cout << "Hello from test.cpp!" << std::endl;
+}
